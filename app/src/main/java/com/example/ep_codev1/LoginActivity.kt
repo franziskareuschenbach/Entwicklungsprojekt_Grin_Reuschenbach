c787561f5f0707c354a56c335f1072627d460580
franziskareuschenbach <franziska_eva.reuschenbach@smail.th-koeln.de>
2021-12-17 17:40:43 +0100

    Firebase anmelden und Login

--- app/src/main/java/com/example/ep_codev1/LoginActivity.kt
+++ app/src/main/java/com/example/ep_codev1/LoginActivity.kt
@@ -1,11 +1,40 @@
 package com.example.ep_codev1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth         //firebase
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        // Initialize Firebase Auth
+        auth = FirebaseAuth.getInstance()        //firebase   Original: auth = Firebase.auth
+
+
+        val keinAccount= findViewById<Button>(R.id.buttonKeinAccount)    // on click event
+
+        keinAccount.setOnClickListener {
+        startActivity(Intent(this, SignUpActivity :: class.java))
+        }
+    }
+
+    //Ist user schon eingeloggt
+    public override fun onStart() {             //firebase
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser       //if(currentUser != null){
+            updateUI(currentUser)               //  reload(); }
+    }
+
+    fun updateUI(currentUser : FirebaseUser?){     //musste hinzugefügt werden
     }
 }
--- app/src/main/java/com/example/ep_codev1/SignUpActivity.kt
+++ app/src/main/java/com/example/ep_codev1/SignUpActivity.kt
@@ -1,11 +1,64 @@
 package com.example.ep_codev1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        auth = FirebaseAuth.getInstance()
+
+        val anmelden = findViewById<Button>(R.id.buttonAnmelden)    // on click event
+
+        anmelden.setOnClickListener {
+            signUpUser()
+        }
+    }
+
+    private fun signUpUser() {
+        val email = findViewById<EditText>(R.id.editTextTextEmailAddress)
+        if (email.text.toString().isEmpty()) {       //kein user name
+            email.error = "Bitte E-Mail eintragen"
+            email.requestFocus()
+            return
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email.text.toString()).matches()) {   //keine richtige EMAIL Adresse
+            email.error = "Bitte eine gültige E-Mail eintragen"
+            email.requestFocus()
+            return
+        }
+
+        var passwort = findViewById<EditText>(R.id.editTextTextPassword)
+        if (passwort.text.toString().isEmpty()) {
+            passwort.error = "Bitte Passwort eintragen"
+            passwort.requestFocus()
+            return
+        }
+
+        auth.createUserWithEmailAndPassword(email.text.toString(), passwort.text.toString())
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    startActivity(Intent(this, LoginActivity :: class.java))
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(baseContext, "Anmeldung fehlgeschlagen",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+
+
+
     }
 }
