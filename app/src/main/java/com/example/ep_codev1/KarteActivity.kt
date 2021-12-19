91da68194b6caf73f031a440856bdff853bd99a1
Emely G <emely.grin@smail.th-koeln.de>
2021-12-19 14:34:45 +0100

    Aenderung fuer Vorlesen

--- app/src/main/java/com/example/ep_codev1/KarteActivity.kt
+++ app/src/main/java/com/example/ep_codev1/KarteActivity.kt
@@ -3,7 +3,6 @@ package com.example.ep_codev1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.ImageButton
 
 class KarteActivity : AppCompatActivity() {
@@ -18,15 +17,15 @@ class KarteActivity : AppCompatActivity() {
             startActivity(Intent(this, AnimationActivity :: class.java))
         }
 
-        /*
-        //VolumeButton zu Vorlesen
-        val volumeButton = findViewById<ImageButton>(R.id.volumebutton)
 
-        volumeButton.setOnClickListener {
-            startActivity(Intent(this, AnimationActivity :: class.java))
+        //VorleseButton zu Vorlesen
+        val vorleseButton = findViewById<ImageButton>(R.id.vorlesebutton)
+
+        vorleseButton.setOnClickListener {
+            startActivity(Intent(this, VorlesenActivity :: class.java))
         }
 
-         */
+
 
 
     }
